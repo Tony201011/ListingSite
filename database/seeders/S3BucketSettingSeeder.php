4fdd2ac52b24fde62d1221cf11ecf19095a6a698
SourabhWadhwa123 <s8811w@gmail.com>
2026-04-02 14:51:01 +0530

    fix s3 bucket  seeder

--- database/seeders/S3BucketSettingSeeder.php
+++ database/seeders/S3BucketSettingSeeder.php
@@ -17,7 +17,8 @@ class S3BucketSettingSeeder extends Seeder
             [
                 'region' => 'auto',
                 'bucket' => 'hotescort',
-                'url' => 'https://1d992bb4fc68899034857c84336a4603.r2.cloudflarestorage.com/hotescort',
+                'url' => 'https://cdn.hotescort.com.au',
+               // 'url' => 'https://1d992bb4fc68899034857c84336a4603.r2.cloudflarestorage.com/hotescort',
                 'endpoint' => 'https://1d992bb4fc68899034857c84336a4603.r2.cloudflarestorage.com',
                 'use_path_style_endpoint' => false,
                 'is_enabled' => true,
