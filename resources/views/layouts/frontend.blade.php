6e08f297153899ba09e49d6ad49fea77e1c23274
SourabhWadhwa123 <s8811w@gmail.com>
2026-04-17 16:42:38 +0530

    fix alphaine js

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -30,6 +30,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <style>[x-cloak] { display: none !important; }</style>
 
     @stack('styles')
 </head>
