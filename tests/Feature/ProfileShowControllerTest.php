8fafa8a28c50217bfdb4ca6f17ffdedbd6b99757
SourabhWadhwa123 <s8811w@gmail.com>
2026-05-04 16:12:58 +0530

    fix assertion code n number of rexcords

--- tests/Feature/ProfileShowControllerTest.php
+++ tests/Feature/ProfileShowControllerTest.php
@@ -369,7 +369,7 @@ class ProfileShowControllerTest extends TestCase
 
         $response = $this->get(route('profile.show', ['slug' => 'jade010-10']));
 
-        $this->assertLessThanOrEqual(0, count($response->viewData('nearbyProfiles')));
+        $this->assertLessThanOrEqual(6, count($response->viewData('nearbyProfiles')));
     }
 
     public function test_each_nearby_profile_has_expected_keys(): void
